c8860753e675539c1692a06965f1b068111e4b5c
deb halder <devhalder321@gmail.com>
2023-07-20 11:48:22 +0530

    3rd

--- src/App.js
+++ src/App.js
@@ -3,8 +3,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      hi
+    <div className="flex flex-col">
+      <div  className="flex flex-col m-0 border-solid border-2 border-indigo-600 items-center fit">
+        <p>Temperature and Pressure </p>
+        <p>Monitor</p>
+
+      </div>
+      <div className="flex ">
+        <div  className="flex flex-col m-3 p-2 border-solid border-2 border-indigo-600
+        items-center fit">
+          <p>Wind box</p>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+        </div>
+        <div  className="flex flex-col m-3 p-2 border-solid border-2 border-indigo-600
+        items-center fit">
+          <p>Temperature</p>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+        </div>
+        <div  className="flex flex-col m-3 p-2 border-solid border-2 border-indigo-600
+        items-center fit">
+          <p>Pressure</p>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+        </div>
+      </div>
+
+
     </div>
   );
 }
